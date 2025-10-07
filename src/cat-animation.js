a3a42a7d91c7a644d9cb29b3917f9a4ca5139a0e
Yiding Hou <yidinghou@gmail.com>
2025-10-06 20:40:02 -0400

    delete unused

--- src/cat-animation.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.cartoon-cat').forEach(cat => {
-    cat.addEventListener('click', function() {
-        cat.classList.remove('jump');
-        void cat.offsetWidth;
-        cat.classList.add('jump');
-    });
-});
